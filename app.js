45e3b9dea55876382c95989ba6378aefe60cabfe
Katerina Kucheriavenko <ekucheryavenko0925@gmail.com>
2021-08-06 15:15:34 +0300

    adds new routers

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const logger = require("morgan");
 const cors = require("cors");
 
 const contactsRouter = require("./routes/api/contacts");
+const authRouter = require("./routes/api/auth");
 
 const app = express();
+
+require("./configs/passport-configs");
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
@@ -13,6 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
+app.use("/api/users", authRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
